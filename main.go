ff411961ebae6c338e4c7a2e78ec0c406b495084
lguzm77 <lguzmm77@icloud.com>
2025-03-23 16:48:45 -0400

    First working version.
    Woops, coded way too fast.

--- main.go
+++ main.go
@@ -3,42 +3,40 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 )
-
-type Permissions int
-
-const READ_WRITE_OWNER_ONLY Permissions = 0755
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("No file path provided")
-		return
+		log.Fatal("file path provided")
+		os.Exit(1)
 	}
 
 	// Accept a file path to a folder as an argument  For now, it has to be an exact path
-	var filepath string = os.Args[1]
+	var directoryPath string = os.Args[1]
 
-	fmt.Println("Changing directory to", filepath)
-	err := os.Chdir(filepath)
+	log.Println("Changing directory to", directoryPath)
+	err := os.Chdir(directoryPath)
 	// Return an error if the error does not come from a filepath known to exist.
 	if err != nil {
-		fmt.Println("Input is not a valid filepath")
-		return
+		log.Fatal("Input is not a valid filepath")
+		os.Exit(1)
 	}
 
 	dataTypesToFolders := map[string]string{
-		"png": "images",
-		"pdf": "pdfs",
-		"jpg": "images",
+		".png": "images",
+		".pdf": "pdfs",
+		".jpg": "images",
 	}
 
 	// create the target folders if these don't exist
 	for _, folder := range dataTypesToFolders {
-		fmt.Println("Checking if target folders exist")
+		log.Println("Checking if target folders exist")
 		_, err := os.Stat(folder)
 		if os.IsNotExist(err) {
-			fmt.Println("Folder does not exists, creating...", folder)
+			log.Println("Folder does not exists, creating...", folder)
 			os.Mkdir(folder, 0755) // Only owner has read, write permissions
 		}
 	}
@@ -47,14 +45,47 @@ func main() {
 
 	// Iterate each file in the current working directory. Move each to the corresponding folder based on its type.
 
-	// Get the current files
+	currentWorkingDirectory, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Get the file type extension
+	files, err := os.ReadDir(directoryPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Move to target folder
+	for _, file := range files {
+		if !file.IsDir() {
+			filename := file.Name()
+			extension := filepath.Ext(filename)
+
+			destinationFolder, exists := dataTypesToFolders[extension]
+
+			if !exists {
+				log.Println("File type not supported", extension)
+				continue
+			}
+			cwd := filepath.Join(currentWorkingDirectory, filename)
+			destination := filepath.Join(currentWorkingDirectory, destinationFolder, filename)
+			move(cwd, destination)
+
+		}
+	}
 
 	// Requirements
 	// navigate to the file path
 	// Inside the folder, based on file type, move to a specific set of folders. e.g .png files go to the images folder
 	// Print that we are done
+}
+
+func move(sourcePath string, targetPath string) {
+	error := os.Rename(sourcePath, targetPath)
+
+	if error != nil {
+		log.Println("Error moving file", error)
+		os.Exit(1)
+	}
+
+	log.Println("File moved", sourcePath)
 }
